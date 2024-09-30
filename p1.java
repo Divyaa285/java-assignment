b42e967ce91497df9a124f5976499d78815dc0a3
Your NameDivya <you@example.comsakundedivya2@gmail.com>
2024-09-30 20:57:14 +0530

    Day 1 code added

---
+++ p1.java
@@ -0,0 +1,15 @@
+class Demo{
+	public static void main (String[]args){
+	int a=0;
+	if(a==0){
+		System.out.println("number is zero");
+		}else if(a>0){
+		System.out.println("number is positive");
+		}else{
+		System.out.println("number is negative");
+		}
+	  }
+}
+
+OUTPUT : number is zero
+
---
+++ p8.java
@@ -0,0 +1,11 @@
+class Demo{
+	public static void main (String[]args){
+	int i;
+	for(i=1;i<=15;i++){
+	  if(i%2==1)
+		System.out.print(i+" ");
+		}
+	  }
+}
+
+OUTPUT : 1 3 5 7 9 11 13 15
