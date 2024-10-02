012860971340574eee4a0bab44f123abd41623bb
Your NameDivya <you@example.comsakundedivya2@gmail.com>
2024-10-02 22:25:20 +0530

    Day3 code added

---
+++ Day3/p3.java
@@ -0,0 +1,13 @@
+class Demo{
+		public static void main(String[]args){
+    		for(int a=0;a<=4;a++){
+		if(a%2==0){
+			System.out.print(0+" ");
+			}else{ 
+      			System.out.print(1+" ");
+		    }
+		}
+	}
+}
+
+OUTPUT : 0 1 0 1 0
